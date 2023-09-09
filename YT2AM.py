05ccf8cf140080c47303ceedfd5cc902aa849c8b
Wes Yarber <wesyarber@icloud.com>
2023-09-09 15:19:26 -0500

    fixed a dum typo

--- YT2AM.py
+++ YT2AM.py
@@ -286,7 +286,7 @@ def show_progress_bar():
     progress_bar.pack(padx=10, side="left")
 
 if __name__ == "__main__":
-    ctk.set_appearance_mode("Syste m")
+    ctk.set_appearance_mode("System")
     ctk.set_default_color_theme("blue")
 
     app = ctk.CTk()
